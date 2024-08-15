b4473bbf7f12573e96223ac7fa2fe8927ab3ddb0
Elmer <soliselmer389@gmail.com>
2024-08-14 22:00:10 -0600

    Dashboard agregado y tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import { Dashboard } from "./components/Dashboard"
+import SideBar from "./components/SideBar"
+import { Button } from "./components/ui/button"
+import {
+  TooltipProvider,
+} from "@/components/ui/tooltip"
 
 function App() {
-
   return (
     <>
-      <h1>Hola mundo</h1>
+      <TooltipProvider>
+        <Dashboard />
+      </TooltipProvider>
     </>
   )
 }
 
 export default App
+
+
+
+{/* <div className="flex">
+        <SideBar />
+        <div className="ml-16 p-6">
+          <h1 className="text-2xl font-bold">Contenido principal</h1>
+          <p>Este es el contenido de la página principal.</p>
+        </div>
+      </div> */}
+
+{/* <Button variant="destructive">Destructive</Button> */ }
