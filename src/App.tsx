5a49c528a6495133ed3ca795e3c871665a0204d3
Elmer <soliselmer389@gmail.com>
2024-08-14 22:59:39 -0600

    Dashboard separando responsabilidades

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import { Dashboard } from "./components/Dashboard"
-import SideBar from "./components/SideBar"
-import { Button } from "./components/ui/button"
+
 import {
   TooltipProvider,
 } from "@/components/ui/tooltip"
 
 function App() {
   return (
-    <>
-      <TooltipProvider>
-        <Dashboard />
-      </TooltipProvider>
-    </>
+    <TooltipProvider>
+      <Dashboard />
+    </TooltipProvider>
   )
 }
 
@@ -19,12 +16,3 @@ export default App
 
 
 
-{/* <div className="flex">
-        <SideBar />
-        <div className="ml-16 p-6">
-          <h1 className="text-2xl font-bold">Contenido principal</h1>
-          <p>Este es el contenido de la página principal.</p>
-        </div>
-      </div> */}
-
-{/* <Button variant="destructive">Destructive</Button> */ }
