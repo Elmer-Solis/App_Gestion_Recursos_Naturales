f728c5a1f0bf78c0bf46d2e8319e81f21142dc5e
Elmer <soliselmer389@gmail.com>
2024-10-02 18:58:01 -0600

    dashboard add fontaneros y bombas

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,25 +1,16 @@
-
 import {
     Activity,
     CreditCard,
     DollarSign,
     Users,
-} from "lucide-react"
+} from "lucide-react";
 
-import {
-    Avatar,
-    AvatarFallback,
-    AvatarImage,
-} from "@/components/ui/avatar"
-import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
 import {
     Card,
     CardContent,
-    CardDescription,
     CardHeader,
     CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 
 import {
     Table,
@@ -28,18 +19,20 @@ import {
     TableHead,
     TableHeader,
     TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
+import { useFontaneroStore } from "@/store/storeFontanero";
+import { useBombaStore } from "@/store/storeBombas";
 
+// Importamos los hooks de Zustand
 
 export function Inicio() {
-    return (
+    const fontaneros = useFontaneroStore((state) => state.fontaneros); // Obtenemos los fontaneros de la store
+    const bombas = useBombaStore((state) => state.bombas); // Obtenemos las bombas de la store
 
+    return (
         <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
             <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
-
-                <Card x-chunk="dashboard-01-chunk-0"
-
-                >
+                <Card x-chunk="dashboard-01-chunk-0">
                     <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                         <CardTitle className="text-sm font-medium">
                             Total Revenue
@@ -94,249 +87,82 @@ export function Inicio() {
                         </p>
                     </CardContent>
                 </Card>
-
             </div>
 
-
-
-            {/* aqui empieza la otra parte */}
-
-
-
+            {/* Tabla de fontaneros y bombas */}
             <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
-
-
-                <Card
-                    className="xl:col-span-2" x-chunk="dashboard-01-chunk-4"
-                >
-                    <CardHeader className="flex flex-row items-center">
-
-                        <div className="grid gap-2">
-                            <CardTitle>Usuarios Vecinos</CardTitle>
-                            <CardDescription>
-                                Recent transactions from your store.
-                            </CardDescription>
-                        </div>
-
-                        <Button variant={"destructive"} size={"lg"} className="ml-auto gap-1 ">
-
-                            View All
-                            {/* <ArrowUpRight className="h-4 w-4" /> */}
-
-                        </Button>
-
-                    </CardHeader>
-
-                    <CardContent>
-                        <Table>
-                            <TableHeader>
-                                <TableRow>
-                                    <TableHead>Customer</TableHead>
-                                    <TableHead className="hidden xl:table-column">
-                                        Type
-                                    </TableHead>
-                                    <TableHead className="hidden xl:table-column">
-                                        Status
-                                    </TableHead>
-                                    <TableHead className="hidden xl:table-column">
-                                        Date
-                                    </TableHead>
-                                    <TableHead className="text-right">Amount</TableHead>
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody>
-                                <TableRow>
-                                    <TableCell>
-                                        <div className="font-medium">Liam Johnson</div>
-                                        <div className="hidden text-sm text-muted-foreground md:inline">
-                                            liam@example.com
-                                        </div>
-                                    </TableCell>
-                                    <TableCell className="hidden xl:table-column">
-                                        Sale
-                                    </TableCell>
-                                    <TableCell className="hidden xl:table-column">
-                                        <Badge className="text-xs" variant="outline">
-                                            Approved
-                                        </Badge>
-                                    </TableCell>
-                                    <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                                        2023-06-23
-                                    </TableCell>
-                                    <TableCell className="text-right">$250.00</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell>
-                                        <div className="font-medium">Olivia Smith</div>
-                                        <div className="hidden text-sm text-muted-foreground md:inline">
-                                            olivia@example.com
-                                        </div>
-                                    </TableCell>
-                                    <TableCell className="hidden xl:table-column">
-                                        Refund
-                                    </TableCell>
-                                    <TableCell className="hidden xl:table-column">
-                                        <Badge className="text-xs" variant="outline">
-                                            Declined
-                                        </Badge>
-                                    </TableCell>
-                                    <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                                        2023-06-24
-                                    </TableCell>
-                                    <TableCell className="text-right">$150.00</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell>
-                                        <div className="font-medium">Noah Williams</div>
-                                        <div className="hidden text-sm text-muted-foreground md:inline">
-                                            noah@example.com
-                                        </div>
-                                    </TableCell>
-                                    <TableCell className="hidden xl:table-column">
-                                        Subscription
-                                    </TableCell>
-                                    <TableCell className="hidden xl:table-column">
-                                        <Badge className="text-xs" variant="outline">
-                                            Approved
-                                        </Badge>
-                                    </TableCell>
-                                    <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                                        2023-06-25
-                                    </TableCell>
-                                    <TableCell className="text-right">$350.00</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell>
-                                        <div className="font-medium">Emma Brown</div>
-                                        <div className="hidden text-sm text-muted-foreground md:inline">
-                                            emma@example.com
-                                        </div>
-                                    </TableCell>
-                                    <TableCell className="hidden xl:table-column">
-                                        Sale
-                                    </TableCell>
-                                    <TableCell className="hidden xl:table-column">
-                                        <Badge className="text-xs" variant="outline">
-                                            Approved
-                                        </Badge>
-                                    </TableCell>
-                                    <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                                        2023-06-26
-                                    </TableCell>
-                                    <TableCell className="text-right">$450.00</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell>
-                                        <div className="font-medium">Liam Johnson</div>
-                                        <div className="hidden text-sm text-muted-foreground md:inline">
-                                            liam@example.com
-                                        </div>
-                                    </TableCell>
-                                    <TableCell className="hidden xl:table-column">
-                                        Sale
-                                    </TableCell>
-                                    <TableCell className="hidden xl:table-column">
-                                        <Badge className="text-xs" variant="outline">
-                                            Approved
-                                        </Badge>
-                                    </TableCell>
-                                    <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                                        2023-06-27
-                                    </TableCell>
-                                    <TableCell className="text-right">$550.00</TableCell>
-                                </TableRow>
-                            </TableBody>
-                        </Table>
-                    </CardContent>
-
-                </Card>
-
-                {/* aqui funciona todavia */}
                 <Card x-chunk="dashboard-01-chunk-5">
                     <CardHeader>
                         <CardTitle>Fontaneros</CardTitle>
                     </CardHeader>
-                    <CardContent className="grid gap-8">
-                        <div className="flex items-center gap-4">
-                            <Avatar className="hidden h-9 w-9 sm:flex">
-                                <AvatarImage src="/avatars/01.png" alt="Avatar" />
-                                <AvatarFallback>OM</AvatarFallback>
-                            </Avatar>
-                            <div className="grid gap-1">
-                                <p className="text-sm font-medium leading-none">
-                                    Olivia Martin
-                                </p>
-                                <p className="text-sm text-muted-foreground">
-                                    olivia.martin@email.com
-                                </p>
-                            </div>
-                            <div className="ml-auto font-medium">+$1,999.00</div>
-                        </div>
-                        <div className="flex items-center gap-4">
-                            <Avatar className="hidden h-9 w-9 sm:flex">
-                                <AvatarImage src="/avatars/02.png" alt="Avatar" />
-                                <AvatarFallback>JL</AvatarFallback>
-                            </Avatar>
-                            <div className="grid gap-1">
-                                <p className="text-sm font-medium leading-none">
-                                    Jackson Lee
-                                </p>
-                                <p className="text-sm text-muted-foreground">
-                                    jackson.lee@email.com
-                                </p>
-                            </div>
-                            <div className="ml-auto font-medium">+$39.00</div>
-                        </div>
-                        <div className="flex items-center gap-4">
-                            <Avatar className="hidden h-9 w-9 sm:flex">
-                                <AvatarImage src="/avatars/03.png" alt="Avatar" />
-                                <AvatarFallback>IN</AvatarFallback>
-                            </Avatar>
-                            <div className="grid gap-1">
-                                <p className="text-sm font-medium leading-none">
-                                    Isabella Nguyen
-                                </p>
-                                <p className="text-sm text-muted-foreground">
-                                    isabella.nguyen@email.com
-                                </p>
-                            </div>
-                            <div className="ml-auto font-medium">+$299.00</div>
-                        </div>
-                        <div className="flex items-center gap-4">
-                            <Avatar className="hidden h-9 w-9 sm:flex">
-                                <AvatarImage src="/avatars/04.png" alt="Avatar" />
-                                <AvatarFallback>WK</AvatarFallback>
-                            </Avatar>
-                            <div className="grid gap-1">
-                                <p className="text-sm font-medium leading-none">
-                                    William Kim
-                                </p>
-                                <p className="text-sm text-muted-foreground">
-                                    will@email.com
-                                </p>
-                            </div>
-                            <div className="ml-auto font-medium">+$99.00</div>
-                        </div>
-                        <div className="flex items-center gap-4">
-                            <Avatar className="hidden h-9 w-9 sm:flex">
-                                <AvatarImage src="/avatars/05.png" alt="Avatar" />
-                                <AvatarFallback>SD</AvatarFallback>
-                            </Avatar>
-                            <div className="grid gap-1">
-                                <p className="text-sm font-medium leading-none">
-                                    Sofia Davis
-                                </p>
-                                <p className="text-sm text-muted-foreground">
-                                    sofia.davis@email.com
-                                </p>
-                            </div>
-                            <div className="ml-auto font-medium">+$39.00</div>
-                        </div>
+                    <CardContent>
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead>Nombre</TableHead>
+                                    <TableHead>Teléfono</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {fontaneros.length > 0 ? (
+                                    fontaneros.map((fontanero) => (
+                                        <TableRow key={fontanero.id}>
+                                            <TableCell>{fontanero.name}</TableCell>
+                                            <TableCell>{fontanero.phone}</TableCell>
+                                        </TableRow>
+                                    ))
+                                ) : (
+                                    <TableRow>
+                                        <TableCell colSpan={3}>
+                                            No hay fontaneros registrados
+                                        </TableCell>
+                                    </TableRow>
+                                )}
+                            </TableBody>
+                        </Table>
+                    </CardContent>
+                </Card>
+
+                <Card x-chunk="dashboard-01-chunk-6">
+                    <CardHeader>
+                        <CardTitle>Bombas de Agua</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead>Nombre de la Bomba</TableHead>
+                                    <TableHead>Zonas Cubiertas</TableHead>
+                                    <TableHead>Fontaneros Asignados</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {bombas.length > 0 ? (
+                                    bombas.map((bomba) => (
+                                        <TableRow key={bomba.id}>
+                                            <TableCell>{bomba.name}</TableCell>
+                                            <TableCell>{bomba.zonas.join(', ')}</TableCell>
+                                            <TableCell>
+                                                {fontaneros.
+                                                    filter((fontanero) => fontanero.bomba === bomba.id)
+
+                                                    .map((fontanero) => fontanero.name)
+                                                    .join(', ') || 'No hay fontaneros asignados'}
+                                            </TableCell>
+                                        </TableRow>
+                                    ))
+                                ) : (
+                                    <TableRow>
+                                        <TableCell colSpan={3}>
+                                            No hay bombas registradas
+                                        </TableCell>
+                                    </TableRow>
+                                )}
+                            </TableBody>
+                        </Table>
                     </CardContent>
                 </Card>
             </div>
-
         </main>
-
-    )
+    );
 }
