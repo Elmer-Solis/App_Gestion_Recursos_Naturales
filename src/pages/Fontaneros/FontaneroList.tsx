a7be5d6f18701daebf82f2ef2fe62e0356df8a96
Elmer <soliselmer389@gmail.com>
2024-10-25 09:50:32 -0600

    list fontaneros

--- src/pages/Fontaneros/FontaneroList.tsx
+++ src/pages/Fontaneros/FontaneroList.tsx
@@ -13,17 +13,15 @@ export default function FontaneroList() {
     }, [fetchBombas]); // El efecto se ejecuta solo una vez cuando el componente se monta
 
     return (
-
         <div className="md:w-1/2 lg:3/5 md:h-full flex-grow overflow-y-scroll
          dark:scrollbar-thin dark:scrollbar-thumb-[#000000] dark:scrollbar-track-[#0a0a0a] 
          dark:scrollbar-thumb-rounded-lg">
-
             {fontaneros.length ? (
                 <>
                     <h2 className="font-black text-3xl text-center">Listado de Fontaneros</h2>
                     <p className="text-xl mt-5 mb-10 text-center">
                         Administra tus {''}
-                        <span className="text-blue-500 font-bold">Fontaneros</span>
+                        <span className="text-blue-500 font-bold">Fontaneros Disponibles</span>
                     </p>
                     {fontaneros.map(fontanero => (
                         <FontaneroDetails
